93aa5eb955354c6e864a949713ced5c0551cdb66
dobariyapurva2003 <purvadobariya9@gmail.com>
2023-01-01 14:28:13 +0530

    Added little modifications

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import {
   Outlet,
 } from "react-router-dom";
 import Home from "./Home";
+import Login from "./Login";
+import Sign_up from "./Sign_up";
 
+import After_login from "./After_login";
 const App = () => {
   return (
     <Router>
@@ -16,6 +19,9 @@ const App = () => {
             <Outlet />
           </>}>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Sign_up />} />
+          <Route path="/After_login" element={<After_login />} />
         </Route>
       </Routes>
     </Router>
